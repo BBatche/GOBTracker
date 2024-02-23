11fabd91ace709e38c803b08fd81debd2c292245
BBatche <bcb248@gmail.com>
2024-02-22 23:03:24 -0500

    Project Setup

---
+++ GOBTracker/GOBTrackerUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GOBTrackerUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GOBTracker/GOBTrackerUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GOBTrackerUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
