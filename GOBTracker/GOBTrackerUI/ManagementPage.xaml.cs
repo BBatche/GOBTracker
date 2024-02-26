d351c9a4d54c0b65d365c4ae22181d116561a076
BBatche <bcb248@gmail.com>
2024-02-25 19:05:56 -0500

    Management Page Started

---
+++ GOBTracker/GOBTrackerUI/ManagementPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using System.Runtime.Loader;
+namespace GOBTrackerUI
+{
+    public partial class ManagementPage : ContentPage
+    {
+
+        public ManagementPage()
+        {
+            InitializeComponent();
+        }
+
+
+        public void OnCreatePlayerClicked()
+        {
+
+        }
+
+        private void CreatePlayer_Clicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Create Player Clicked");
+        }
+        private void UpdatePlayer_Clicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Create Player Clicked");
+        }
+        private void DeletePlayer_Clicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Create Player Clicked");
+        }
+        private void ViewPlayers_Clicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Create Player Clicked");
+        }
+        private void CreateTeam_Clicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Create Player Clicked");
+        }
+        private void UpdateTeam_Clicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Create Player Clicked");
+        }
+        private void DeleteTeam_Clicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Create Player Clicked");
+        }
+        private void ViewTeams_Clicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Create Player Clicked");
+        }
+
+    }
+
+}
