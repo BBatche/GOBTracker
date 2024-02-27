3483b2aad749a3c19f94e6d09e89b23de9b90ac7
Austin Ressler <aressler03@gmail.com>
2024-02-26 21:09:58 -0500

    created stats page + GetRawStatsAsync() in ApiService

--- GOBTracker/GOBTrackerUI/APIMethods/ApiService.cs
+++ GOBTracker/GOBTrackerUI/APIMethods/ApiService.cs
@@ -15,6 +15,7 @@ namespace GOBTrackerUI.APIMethods
         private readonly HttpClient _httpClient;
         string teamsApiUrl = "https://localhost:7063/api/Teams";
         string teamRosterApiUrl = "https://localhost:7063/api/TeamRoster";
+        string playerGameStatsApiUrl = "https://localhost:7063/api/PlayerGameStats";
         public ApiService()
         {
             _httpClient = new HttpClient();
@@ -86,6 +87,43 @@ namespace GOBTrackerUI.APIMethods
                 }
             }
             return roster;
+        }
+
+
+        async public Task<List<PlayerGameStat>> GetRawStatsAsync(String lastNameSearch)
+        {
+            string apiUrl = playerGameStatsApiUrl;
+            List<PlayerGameStat> rawStats = null;
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonString = await response.Content.ReadAsStringAsync();
+                        rawStats = JsonConvert.DeserializeObject<List<PlayerGameStat>>(jsonString);
+                        
+                        rawStats = rawStats.Where(x => x.LastName.Equals(lastNameSearch)).ToList();
+                        //rawStats = rawStats.Where(x => x.LastName.Equals("hi")).ToList();
+
+                    }
+                    else
+                    {
+                        Debug.WriteLine("API request failed with status code: " + response.StatusCode);
+                        return null;
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Error: " + ex.Message);
+                    return null;
+                }
+            }
+            return rawStats;
         }
 
 
