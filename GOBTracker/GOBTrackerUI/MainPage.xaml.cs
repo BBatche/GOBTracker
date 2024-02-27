06dd4369555dc50e98252ae9d4d72ca0b33340e4
BBatche <bcb248@gmail.com>
2024-02-26 20:18:19 -0500

    Revert "initialized stats page"
    This reverts commit 340a277cd2303089116b132d553e96c90f8e22a0.

--- GOBTracker/GOBTrackerUI/MainPage.xaml.cs
+++ GOBTracker/GOBTrackerUI/MainPage.xaml.cs
@@ -13,28 +13,12 @@
         {
             count++;
 
-<<<<<<< Updated upstream
             if (count == 1)
                 CounterBtn.Text = $"Clicked {count} time";
             else
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
-=======
-        private void ScoreKeeping_Clicked(object sender, EventArgs e)
-        {
-
-        }
-
-        private void GameView_Clicked(object sender, EventArgs e)
-        {
-
-        }
-
-        private async void Statistics_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new StatModePage());
->>>>>>> Stashed changes
         }
     }
 
